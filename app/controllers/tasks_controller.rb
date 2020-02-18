9c81365af5e0b41d71b83c5c2b270a6b77b9ecd0
guillaume veldekens <guillaume.veldekens@gmail.com>
2020-02-18 17:15:38 +0100

    Done

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,36 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params_task)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params_task)
+    redirect_to tasks_path
+  end
+
+  private
+
+  def params_task
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
